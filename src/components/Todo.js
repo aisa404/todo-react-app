e6ad0a30dc0f3c7c8ca6bff138f3919645612432
Aiša Muratspahić <aisa.m2223@gmail.com>
2023-06-25 02:41:05 +0200

    generate and display a todo for each value in the state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export const Todo = () => {
+
+export const Todo = ({task}) => {
   return (
-    <div>Todo</div>
+    <div className='Todo'>
+        <p>{task.task}</p>
+        <div >
+            <FontAwesomeIcon icon={faPenToSquare}/>
+            <FontAwesomeIcon icon={faTrash}/>
+        </div>
+    </div>
   )
 }
